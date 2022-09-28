f32aa22286d4ac75cb49735333ae0f8e778346e1
Gerardo Miranda <gerardodev25@gmail.com>
2022-09-27 20:46:17 -0400

    Configuración de Webpack 5 con loaders y estilos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 
+import '../styles/components/app.css';
+
 export const App = () => <h1>Hola Mundo!!</h1>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
@@ -22,12 +24,24 @@ module.exports = {
         test: /\.html$/,
         use: [{ loader: 'html-loader' }],
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
     }),
   ],
   devServer: {
