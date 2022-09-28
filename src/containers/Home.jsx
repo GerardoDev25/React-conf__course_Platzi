f57ea40c2708f8833ed5aaea88b4ed146392c967
Gerardo Miranda <gerardodev25@gmail.com>
2022-09-27 21:34:58 -0400

    Maquetación y estilos de la lista de productos

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { Products } from '../components/Products';
+import { initialState } from '../initialState';
 
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  return <Products products={initialState.products} />;
+};
